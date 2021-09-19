696fafb3322fc72a39b746f2d306b69dbe21e4a8
K A R T H I K <karthik.lal558@gmail.com>
2021-09-19 15:38:52 +0530

    Github-page: update #main_bg and cleanup some codes
    * enable hue_rotate for social_hover on #mainbg
    
    Signed-off-by: K A R T H I K <karthik.lal558@gmail.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/**
-* Template Name: Personal - v4.1.0
-* Template URL: https://bootstrapmade.com/personal-free-resume-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 (function() {
   "use strict";
 
