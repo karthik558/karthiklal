e25f587a49db9f45f25441ce2a08f7038f9fa8d6
K A R T H I K <karthik.lal558@gmail.com>
2022-03-01 15:58:02 +0530

    portfolio: edit mail-sending-action
    Signed-off-by: K A R T H I K <karthik.lal558@gmail.com>

--- php/mail.php
+++ php/mail.php
@@ -5,13 +5,15 @@
     $email= 'karthik.lal558@gmail.com';
     $text= $_POST["message"];
     $phone= $_POST["phone"];
+    $subject= $_POST["subject"];
+
 
 
     $headers = 'MIME-Version: 1.0' . "\r\n";
     $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
-    $message ='hello';
+    $message ='hello<br>subject:'.$subject.'<br>phone:'.$phone.'<br>message:'.$text;
 
     if (@mail($to, $email, $message, $headers))
     {
