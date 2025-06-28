c514798e3119cf1dc18fcf0142de50fe147a2fd5
KARTHIK LAL <dev@karthiklal.in>
2025-06-28 11:33:25 +0530

    portfolio: fix(command-dialog): Add aria-labelledby to DialogContent for accessibility

--- components/ui/command.tsx
+++ components/ui/command.tsx
@@ -27,9 +27,9 @@ Command.displayName = CommandPrimitive.displayName
 const CommandDialog = ({ children, ...props }: DialogProps) => {
   return (
     <Dialog {...props}>
-      <DialogContent className="overflow-hidden p-0 shadow-lg">
+      <DialogContent className="overflow-hidden p-0 shadow-lg" aria-labelledby="command-dialog-title">
         <VisuallyHidden.Root>
-          <DialogTitle>Command Menu</DialogTitle>
+          <DialogTitle id="command-dialog-title">Command Menu</DialogTitle>
         </VisuallyHidden.Root>
         <Command className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
           {children}
